58e57144751a5a80fa9c108af3231bccb150f596
David Lucero <lucerodavid@att.net>
2024-12-17 15:01:17 -0600

    add insta controller and router

--- src/controller/researcher_controller.py
+++ src/controller/researcher_controller.py
@@ -1,28 +1,11 @@
-from fastapi import FastAPI, HTTPException
-from fastapi.middleware.cors import CORSMiddleware
+from fastapi import APIRouter, HTTPException
 from pydantic import BaseModel
-from uvicorn import Config, Server
 from src.researcher.agents.data_gatherer import DataGatherer
 from src.researcher.agents.newsapi_agent import NewsApiAgent
-import json
-import os
-import asyncio
 
-app = FastAPI()
 
-origins = [
-    "http://localhost",
-    "http://localhost:3000",  # Add your frontend URL here
-    # Add other allowed origins if needed
-]
 
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
+router = APIRouter()
 
 # Initialize the DataGatherer service
 data_gatherer = DataGatherer()
@@ -33,7 +16,7 @@ class TopicRequest(BaseModel):
     topic: str
     source: str = "default_source"
 
-@app.post("/gather_data")
+@router.post("/gather_data")
 async def gather_data(request: TopicRequest):
     if request.source in ["news", "youtube"]:
         raise HTTPException(status_code=400, detail=f"The source '{request.source}' is not implemented.")
@@ -48,30 +31,6 @@ async def gather_data(request: TopicRequest):
     return result
 
 
-class ProactorServer(Server):
-    def run(self, sockets=None):
-        loop = asyncio.ProactorEventLoop()
-        asyncio.set_event_loop(loop) # since this is the default in Python 3.10, explicit selection can also be omitted
-        asyncio.run(self.serve(sockets=sockets))
-
-if __name__ == '__main__':
-    config = Config(app=app, host="0.0.0.0", port=8000, reload=True)
-    server = ProactorServer(config=config)
-    server.run()
 
 
 
-
-# if __name__ == "__main__":
-#     if os.name == "nt":
-#         loop = asyncio.ProactorEventLoop()
-#         asyncio.set_event_loop(loop)
-#         # asyncio.set_event_loop_policy(asyncio.WindowsProactorEventLoopPolicy())
-#     import uvicorn
-#     uvicorn.run(app, host="0.0.0.0", port=8000, reload=False)
-#     #     uvicorn.run(
-#     #     "test:app",
-#     #     host="0.0.0.0",
-#     #     reload=False,
-#     #     port=8002
-#     # )
