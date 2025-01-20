c80135226bc113f106ad46c7c562541a4ec04107
David Lucero <lucerodavid@att.net>
2025-01-20 16:16:42 -0600

    p5 fincally working

--- portal/src/Components/InteractiveTimelineComponent.js
+++ portal/src/Components/InteractiveTimelineComponent.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect, useRef } from 'react';
 
 import p5 from 'p5';
+import { use } from 'react';
 
 
 const InteractiveTimelineComponent = ({userId, showTimeline}) => {
@@ -14,10 +15,9 @@ const InteractiveTimelineComponent = ({userId, showTimeline}) => {
     const [openDialog, setOpenDialog] = useState(false);
     const [myP5, setMyP5] = useState(null);
     const [timeSlider, setTimeSlider] = useState(null);
-    const [images, setImages] = useState([]);
-    const [currentTitle, setCurrentTitle] = useState(null);
-    const [currentBody, setCurrentBody] = useState(null);
-    const [currentImgID, setCurrentImgID] = useState(null);
+
+
+
 
     useEffect(() => {
         console.log("TimelineComponent mounted");
@@ -33,125 +33,125 @@ const InteractiveTimelineComponent = ({userId, showTimeline}) => {
         };
     }, []);
 
+
+
     useEffect(() => {
-        if (showTimeline && userId && !myP5) {
-            console.log("Creating timeline...");
-            setUpP5();
+
+        let currentTitle = "";
+        let currentBody = "";
+        let currentImgID = null;
+        let images = [];
+        let ts = null;
+        let sliderValue = 25;
+
+        const displayYear = (p, ts) => {
+            p.fill(0);
+            p.textAlign(p.CENTER);
+            p.noStroke();
+            p.textSize(40);
+            p.text(1750 + p.int(ts.value()), p.width / 2, 50);
         }
-
-    }, [showTimeline, userId, myP5]);
-
-
-
-
-    const createSketch = () => {
         
-        const sketch = (p) => {
-            const ts = p.createSlider(0, 50, 25, 0.1)
+        const displayText = (p) => {
 
-            p.preload = () => {
-                const imgs = [p.loadImage("timeline_images/some_image.png")]
-                setImages(imgs);
+            p.textSize(20);
+            p.text(currentTitle, 0, 60, 360, 90);
+            p.textSize(16);
+            p.textAlign(p.LEFT);
+            p.text(currentBody, 0, 140, 360, 640);
+        }
+        
+        const displayLine = (p) => {
+            p.stroke(0);
+            p.strokeWeight(3);
+            p.line(0, 350, p.width, 350);
+        }
+        
+        const displayIncrements = (p, ts) => {
+            //draws vertical marks on line to convey movement
+            p.stroke(0);
+            for (var i = 0; i < 51; i++){
+                p.line(i * 25 - (ts.value() * 400) % 400, 340, i * 25 - (ts.value() * 400) % 400, 360);
             }
-            p.setup = () => {
-                p.createCanvas(screenWidth, screenHeight);
-                p.background(255);
-                ts.position(screenWidth / 2 - 362.5, screenHeight - 20); //center timeline
-                ts.style('width', '700px');
-                setTimeSlider(ts);
-                console.log("timeSlider: ",timeSlider);
-            };
-            p.draw = () => {
-                console.log("timeSlider in draw: ", ts);
-               
-                displayLine(p, ts); //horizontal line in timeline
-                displayYear(p, ts); //year display on top
-                displayText(p, ts); //displays title and body on left
-                displayImage(p);
-                displayIncrements(p, ts);
-                  //put events on line. see event function.
-                event(p,'The French and Indian War Ends\nFeb 10 1763',
-                    'The French and Indian War ends. The UK is heavily in debt and begins to expect the colonies to help pay that debt, since the war resuced them from the French. This is important because the citizens of the US had no say in how they would be charged.',
-                    1763, 0, false, ts);
-                // event('Stamp Act\nMar 22 1765',
-                //     'Parliament imposes the Stamp Act on the colonies. Major protests against "taxation without representation" start. This is important because the citizens of the US resisted unfair taxation for the first significant time',1765,1,true);
-            
-            };
-        };
-        return sketch;
-    }
+                
+        }
 
-    const setUpP5 = () => {
-        const sketch = createSketch();
-        const container = sketchRef.current;
-        const myP5 = new p5(sketch, container)
+        const createSketch = () => {
         
-        setMyP5(myP5);
-    };
-
-    const displayYear = (p, ts) => {
-        p.fill(0);
-        p.textAlign(p.CENTER);
-        p.noStroke();
-        p.textSize(40);
-
-        p.text(1750 + p.int(ts.value()), p.width / 2, 50);
-    }
+            return (p) => {
     
-    const displayText = (p) => {
-        p.textSize(20);
-        p.text("title", 0, 60, 360, 90);
-        p.textSize(16);
-        p.textAlign(p.LEFT);
-        p.text("body", 0, 140, 360, 640);
-    }
+                p.preload = () => {
+                    const imgs = [p.loadImage("timeline_images/some_image.png")]
+                    images = imgs;
+                }   
+                p.setup = () => {
+                    p.createCanvas(screenWidth, screenHeight);
+                    ts = p.createSlider(0, 50, 25, 1);
+                    ts.input(() => {
+                        sliderValue = ts.value();
+                    });
     
-    const displayLine = (p) => {
-        p.stroke(0);
-        p.strokeWeight(3);
-        p.line(0, 350, p.width, 350);
-    }
+                    ts.position(screenWidth / 2 - 362.5, screenHeight - 20); //center timeline
+                    ts.style('width', '700px');
+                    setTimeSlider(ts);
+                };
+                p.draw = () => {
+                    p.background(255);
     
-    const displayIncrements = (p, ts) => {
-        //draws vertical marks on line to convey movement
-        p.stroke(0);
-        for (var i = 0; i < 51; i++){
-            p.line(i * 25 - (ts.value() * 400) % 400, 340, i * 25 - (ts.value() * 400) % 400, 360);
-        }
+                    displayLine(p, ts); //horizontal line in timeline
+                    displayYear(p, ts); //year display on top
+                    
+                    displayImage(p);
+                    displayIncrements(p, ts);
+                      //put events on line. see event function.
+                    event(p,'The French and Indian War Ends\nFeb 10 1763',
+                        'The French and Indian War ends. The UK is heavily in debt and begins to expect the colonies to help pay that debt, since the war resuced them from the French. This is important because the citizens of the US had no say in how they would be charged.',
+                        1763, 0, false, ts);
+                    // event('Stamp Act\nMar 22 1765',
+                    //     'Parliament imposes the Stamp Act on the colonies. Major protests against "taxation without representation" start. This is important because the citizens of the US resisted unfair taxation for the first significant time',1765,1,true);
+                    
+                    displayText(p, ts); //displays title and body on left
+                };
+            };
             
-    }
-  
-    function event(p,caption, body, year, imageID, up, ts) {
-        let xOffs = p.width / 2 + (year-1775)*400 + (25 - ts.value()) * 400; //offset to line up with moving line
-        p.noStroke();
-        p.textSize(12);
-        p.textAlign(p.CENTER);
-        p.text(caption, xOffs, up ? 320 : 380); //puts caption above or below line
-        p.fill(0, 115, 200);
-        p.ellipse(xOffs, 350, 15, 15); //blue mouseover thingy
-        if (p.dist(p.mouseX, p.mouseY, xOffs, 350) < 18) { //checks if mouse is close (20px) to blue thingy
-          //update information being displayed
-        //   currentTitle = caption;
-        //   currentBody = body;
-        //   currentImgID = imageID;
-            setCurrentTitle(caption);
-            setCurrentBody(body);
-            setCurrentImgID(imageID);
         }
-        p.fill(0);
-      }
+        const event = (p, caption, body, year, imageID, up, ts) => {
+            let xOffs = p.width / 2 + (year-1775)*400 + (25 - ts.value()) * 400; //offset to line up with moving line
+            p.noStroke();
+            p.textSize(12);
+            p.textAlign(p.CENTER);
+            p.text(caption, xOffs, up ? 320 : 380); //puts caption above or below line
+            p.fill(0, 115, 200);
+            p.ellipse(xOffs, 350, 15, 15); //blue mouseover thingy
+            if (p.dist(p.mouseX, p.mouseY, xOffs, 350) < 50) { //checks if mouse is close (20px) to blue thingy
+                currentTitle = caption;
+                currentBody = body;
+                currentImgID = imageID;
+            }
+            p.fill(0);
+          }
+    
+          const displayImage = (p) => {
+            p.imageMode(p.CENTER);
+            if (currentImgID != null) //check if any image has been selected
+              if (images[currentImgID].width > images[currentImgID].height)
+                //if width bigger, scale based on amount width decreased
+                p.image(images[currentImgID], 540, 170, 200, (200 / images[currentImgID].width) * images[currentImgID].height);
+              else
+                //if height bigger, scale based on amount height decreased
+                p.image(images[currentImgID], 540, 170, (200 / images[currentImgID].height) * images[currentImgID].width, 200);
+          }
+          
+          const sketch = createSketch();
+          const container = sketchRef.current;
+          const myP5 = new p5(sketch, container)
 
-      function displayImage(p) {
-        console.log("Center: " + p.CENTER)
-        p.imageMode(p.CENTER);
-        if (currentImgID != null) //check if any image has been selected
-          if (images[currentImgID].width > images[currentImgID].height)
-            //if width bigger, scale based on amount width decreased
-            p.image(images[currentImgID], 540, 170, 200, (200 / images[currentImgID].width) * images[currentImgID].height);
-          else
-            //if height bigger, scale based on amount height decreased
-            p.image(images[currentImgID], 540, 170, (200 / images[currentImgID].height) * images[currentImgID].width, 200);
-      }
+        return () => {
+            myP5.remove();
+          };
+    }, []);
+
+
 
     if (userId === null) return <div>Please select a user to view their timeline.</div>;
     if (showTimeline === null || showTimeline === undefined || showTimeline === false) return <div>Please select a user to view their timeline.</div>;
